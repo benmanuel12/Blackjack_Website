bb3a7660686fd69ccb8ff5b87080e87880709888
Benjamin Manuel <contact.ben.manuel@gmail.com>
2019-11-30 16:18:14 +0000

    added html validation

--- www/login.php
+++ www/login.php
@@ -11,21 +11,21 @@ outputNavBar();
 
 <!--Main Body-->
 <div id=mainbodylogin>
-        <form>
-            Username: <br>
-            <input id=username type="text" name="username"><br> <br> Password: <br>
-            <input id=password type="text" name="password"><br> <br>
-            <button type="button" onclick="login()">Log In</button> 
-        </form>
-        <div id=sidebar>
-            <div id=button>
-                Sign in instead?
-            </div>
-            <div id=logininfo>
-                Use this form to log into a pre-existing account so that your game scores can be tracked. High scores will be added to the Rankings page. Games played while not logged in will not be tracked.
-            </div>
+    <form>
+        Username: <br>
+        <input id=username type="text" name="username"><br> <br> Password: <br>
+        <input id=password type="password" name="password"><br> <br>
+        <button type="button" onclick="login()">Log In</button>
+</form>
+    <div id=sidebar>
+        <div id=button>
+            Sign in instead?
+        </div>
+        <div id=logininfo>
+            Use this form to log into a pre-existing account so that your game scores can be tracked. High scores will be added to the Rankings page. Games played while not logged in will not be tracked.
         </div>
     </div>
+</div>
     
 <?php
 
--- www/signup.php
+++ www/signup.php
@@ -10,24 +10,24 @@ outputNavBar();
 
 <!--Main Body-->
 <div id=mainbodysignup>
-        <form>
-            Username: <br>
-            <input id=username type="text" name="username"><br> <br> Password: <br>
-            <input id=password type="text" name="password"><br> <br> Repeat Password: <br>
-            <input id=repeatpassword type="text" name="password"><br> <br> Email Address: <br>
-            <input id=email type="text" name="email"><br> <br> Phone Number <br>
-            <input id=number type="text" name="number"><br>
-            <button type="button" onclick="signup()">Sign Up</button>
-        </form>
-        <div id=sidebar>
-            <div id=button>
-                Log in instead?
-            </div>
-            <div id=signininfo>
-                Use this form to sign up to the website and create an new player account. This allows the website to track your scores across games and update the Rankings page with high scores.
-            </div>
+    <form>
+        Username: <br>
+        <input id=username type="text" name="username"><br> <br> Password: <br>
+        <input id=password type="password name="password"><br> <br> Repeat Password: <br>
+        <input id=repeatpassword type="password" name="password"><br> <br> Email Address: <br>
+        <input id=email type="email" name="email"><br> <br> Phone Number <br>
+        <input id=number type="tel" name="number"><br>
+        <button type="button" onclick="signup()">Sign Up</button>
+</forn>
+    <div id=sidebar>
+        <div id=button>
+            Log in instead?
+        </div>
+        <div id=signininfo>
+            Use this form to sign up to the website and create an new player account. This allows the website to track your scores across games and update the Rankings page with high scores.
         </div>
     </div>
+</div>
 
 <?php
 //Output the footer and closing HTML tags
